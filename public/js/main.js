6ddce3fd8db49fe43a1bcdee5c4f4682633f855c
lavelee <lavelee15@gmail.com>
2016-12-05 19:02:18 +0900

    main page 완성, 서버 완성, 레이아웃 잡음

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+function whenActived(start_el,target_el,class_toggle){
+  jQuery(start_el).hover(function(){
+    jQuery(target_el).toggleClass(class_toggle)
+    console.log("sdddfsdf")
+  })}
+
+whenActived('.layout-menu-nav','.menu-sub','submenu-active')
+
+
+jQuery(document).ready(function(){
+  jQuery('.slide-slick').slick({
+    'autoplay': true,
+    'autoplaySpeed': 2000,
+    'dots': true,
+    'arrows': false
+  });
+});
